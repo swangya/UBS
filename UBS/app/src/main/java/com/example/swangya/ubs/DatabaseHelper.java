31a5c464f61b65e95ad937eb529d4932a37a0b33
swangya <swangya@gmail.com>
2016-12-04 14:00:34 -0600

    Loging funcationality changed

--- UBS/app/src/main/java/com/example/swangya/ubs/DatabaseHelper.java
+++ UBS/app/src/main/java/com/example/swangya/ubs/DatabaseHelper.java
@@ -13,27 +13,31 @@ import android.view.View;
 
 import static android.R.attr.id;
 
+import static android.R.attr.id;
+
+
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "Register.db";
-    private static final String TABLE_NAME = "register_table";
-    private static final String USER_NUM = "USERNUM";
-    private static final String FIRST_NAME = "FIRSTNAME";
-    private static final String LAST_NAME = "LASTNAME";
-    private static final String ID = "ID";
-    private static final String DOB = "DOB";
-    private static final String PASSWORD = "PASSWORD";
-    private static final String RETYPEPASSWORD = "RETYPEPASSWORD";
-    private static final String TYPE = "TYPE";
-    private static final String SECURITY_QUESTION1 = "QUESTIONI";
-    private static final String SECURITY_QUESTION2 = "QUESTIONII";
-    private static final String SECURITY_QUESTION3 = "QUESTIONIII";
+
+    public static final String DATABASE_NAME = "Register.db";
+    public static final String TABLE_NAME = "register_table";
+    public static final String USER_NUM = "USERNUM";
+    public static final String FIRST_NAME = "FIRSTNAME";
+    public static final String LAST_NAME = "LASTNAME";
+    public static final String ID = "ID";
+    public static final String PASSWORD = "PASSWORD";
+    public static final String RETYPEPASSWORD = "RETYPEPASSWORD";
+    public static final String TYPE = "TYPE";
+    public static final String SECURITY_QUESTION1 = "QUESTIONI";
+    public static final String SECURITY_QUESTION2 = "QUESTIONII";
+    public static final String SECURITY_QUESTION3 = "QUESTIONIII";
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " +  TABLE_NAME + " (USERNUM INTEGER PRIMARY KEY AUTOINCREMENT, FIRSTNAME TEXT, LASTNAME TEXT, ID TEXT, DOB TEXT, PASSWORD TEXT, RETYPEPASSWORD TEXT, TYPE TEXT, QUESTIONI TEXT, QUESTIONII TEXT, QUESTIONIII TEXT)");
+        db.execSQL("create table " +  TABLE_NAME + " (USERNUM INTEGER PRIMARY KEY AUTOINCREMENT, FIRSTNAME TEXT, LASTNAME TEXT, ID TEXT, PASSWORD TEXT, RETYPEPASSWORD TEXT, TYPE TEXT, QUESTIONI TEXT, QUESTIONII TEXT, QUESTIONIII TEXT)");
 
     }
 
@@ -43,14 +47,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
 
     }
-    public boolean insertData(String firstname, String lastname, String Id, String dob, String password, String repassword, String type, String q1, String q2, String q3)
+    public boolean insertData(String firstname, String lastname, String Id, String password, String repassword, String type, String q1, String q2, String q3)
     {
         SQLiteDatabase db= this.getWritableDatabase();
         ContentValues contentValues=new ContentValues();
         contentValues.put(FIRST_NAME, firstname);
         contentValues.put(LAST_NAME, lastname);
         contentValues.put(ID, Id);
-        contentValues.put(DOB, dob);
         contentValues.put(PASSWORD, password);
         contentValues.put(RETYPEPASSWORD, repassword);
         contentValues.put(TYPE, type);
@@ -66,9 +69,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
     public Cursor getData()
     {
-        SQLiteDatabase db= this.getWritableDatabase();
+        SQLiteDatabase db= this.getReadableDatabase();
         Cursor res = db.rawQuery("select * from "+ TABLE_NAME, null);
         return res;
     }
 
+    public Cursor logindata() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns= {ID,PASSWORD,FIRST_NAME};
+        Cursor res = db.query(TABLE_NAME,columns,null,null,null,null,null);
+        return res;
+    }
+    public Cursor forgotPassword()
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns= {FIRST_NAME,LAST_NAME,SECURITY_QUESTION1,SECURITY_QUESTION2,SECURITY_QUESTION3,PASSWORD,ID};
+        Cursor res = db.query(TABLE_NAME,columns,null,null,null,null,null);
+        return res;
+    }
 }
